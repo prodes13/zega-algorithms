51c21e4e1d0b6ac74b40109129d27290e7d24601
prodes13 <ciobotaru.florin@gmail.com>
2022-10-16 00:06:40 +0300

    Add binary search

--- binary-search.js
+++ binary-search.js
@@ -1,10 +1,26 @@
 let canvas, ctx;
 const CANVAS_WIDTH = 950;
 const CANVAS_HEIGHT = 600;
-const TIME_BETWEEN_FRAMES = 2000;
-let selectedNumber;
+const X_START = 70,
+    Y_START = 70,
+    X_INTERVAL = 50,
+    X_INTERVAL_TEXT = 35;
 
-let array = [{
+let selectedNumber;
+let nextBtn = document.querySelector("#next");
+let startBtn = document.querySelector("#start");
+
+let statistics = {
+    low: 0,
+    high: 0,
+    mid: 0,
+    guess: 0,
+    steps: 0
+};
+let numberToGuess = 0, message ="";
+
+let array = [
+    {
         value: -30,
         selected: false
     },
@@ -43,82 +59,99 @@ let array = [{
     {
         value: 20,
         selected: false
+    },
+    {
+        value: 24,
+        selected: false
+    },
+    {
+        value: 30,
+        selected: false
+    },
+    {
+        value: 32,
+        selected: false
     }
 ];
 
-function init() {
-    canvas = document.getElementById('canvas');
-    var binSearch = binarySearch(array);
-    console.log(binSearch)
+canvas = document.getElementById('canvas');
+let binSearch;
+nextBtn.disabled = true;
+draw();
 
-    function anim() {
-        draw();
-        binSearch.next(); // call next iteration of the bubbleSort function
-    }
-    anim();
-    setInterval(anim, TIME_BETWEEN_FRAMES);
-}
+startBtn.addEventListener("click", function() {
+    numberToGuess = document.querySelector("#numberToGuess").value;
+    binSearch = binarySearch(array, numberToGuess);
+    draw();
+    nextBtn.disabled = false;
+    this.disabled = true;
+});
 
-function showNumbers() {
-    console.log("Showing numbers");
-    let ulContainer = document.getElementById("showNumbers");
-    let elementToCopy = document.querySelector("#copyElement");
-    for (let i = 0; i < array.length; i++) {
-        // ulContainer.innerHTML += `<li class="page-item"><a class="page-link" href="#">${array[i].value}</a></li>`;
-        let = newElement = elementToCopy.cloneNode(true);
-        newElement.setAttribute("id", array[i].value);
-        newElement.style.display = "inline-block";
-        let a = document.createElement('a');
-        newElement.appendChild(a);
-        a.setAttribute("class", "page-link");
-        a.innerText = array[i].value;
-        a.onclick = function () {
-            console.log(a.innerText);
-            selectedNumber = Number(a.innerText);
-            init();
-        }
-        // console.log(link);
-        // link.innerHTML = array[i].value;
-        ulContainer.appendChild(newElement);
+
+nextBtn.addEventListener("click", () => {
+    binSearch.next();
+    draw();
+});
+
+function getPosition(i, j) {
+    return {
+        x: X_START * i + X_INTERVAL,
+        y: Y_START * j
     }
 }
 
 function draw() {
     if (canvas.getContext) {
         var ctx = canvas.getContext('2d');
-
         ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
         for (var i = 0; i < array.length; i++) {
             ctx.font = "15px Arial";
             ctx.fillStyle = array[i].selected ? "#FF0000" : "#0000ff";
-            ctx.fillText(array[i].value, 70 * i, 50);
-            ctx.fillRect(70 * i, CANVAS_HEIGHT / 2, 10, -array[i].value * 10);
+            let x = X_START * i + X_INTERVAL;
+            let y = Y_START;
+            ctx.fillText(array[i].value, x, y);
+            ctx.strokeStyle = array[i].selected ? "#FF0000" : "#0000ff";
+            ctx.strokeRect(x - 15, y - 30, 50, 50);
+            ctx.font = "15px Arial";
+            ctx.fillStyle = "#000000";
+
+            statistics.low !== statistics.high && statistics.low !== statistics.mid && ctx.fillText("LOW: " + statistics.low, statistics.low * X_START + X_INTERVAL_TEXT, Y_START * 1.7);
+            statistics.low !== statistics.high && statistics.mid !== statistics.high &&ctx.fillText("HIGH: " + statistics.high, statistics.high * X_START + X_INTERVAL_TEXT, Y_START * 1.7);
+            statistics.mid !== 0 && ctx.fillText("MID: " + statistics.mid,  statistics.mid * X_START + X_INTERVAL_TEXT, Y_START * 1.7);
+            statistics.guess !== 0 && ctx.fillText("GUESS: " + statistics.guess, statistics.mid * X_START + X_INTERVAL_TEXT, Y_START * 2);
+            statistics.steps !== 0 && ctx.fillText("STEPS: " + statistics.steps, X_INTERVAL_TEXT, Y_START * 2.4);
+            ctx.fillStyle = "#a83632";
+            message && ctx.fillText(message, X_INTERVAL_TEXT, Y_START * 2.8);
+            ctx.fillText(i, x, y - 40);
         }
     }
 }
 
-function* binarySearch(array) { // * is magic
-    let numberToFind = selectedNumber;
-    let iterator = 0;
-    let maxIndex = array.length - 1;
-    let index;
-    while (iterator <= maxIndex) {
-        index = Math.floor(iterator + (maxIndex - iterator) / 2);
-        array[index].selected = true;
-
-        if (array[index].value < numberToFind) {
-            iterator = index + 1;
-        } else if (array[index].value == numberToFind) {
-            console.log('!!!!array is at ', index);
-            return index;
+function* binarySearch(list, item) {
+    let low = 0;
+    let high = list.length - 1;
+    let steps = 0;
+    statistics = {low, high, steps: 0};
+    while( low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let guess = list[mid].value;
+        statistics = {
+            low,
+            high,
+            mid,
+            guess,
+            steps: ++steps
+        };
+        list[mid].selected = true;
+        if (guess == item) {
+            return mid;
+        }
+        if (guess > item) {
+            high = mid - 1;
         } else {
-            maxIndex = index - 1;
+            low = mid + 1;
         }
-        yield iterator;
+        yield low;
     }
-    console.log('Not found!');
-
-    // return -1;
+    message = "NOT FOUND !!!"
 }
-
-showNumbers();
